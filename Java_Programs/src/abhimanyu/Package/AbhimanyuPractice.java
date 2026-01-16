6a6d5e3b27d5baa5b4f5d282b61af3cb8a4cb14c
abhimanyuray794-lgtm <abhimanyuray794@gmail.com>
2026-01-16 16:52:57 +0530

    added new message

--- Java_Programs/src/abhimanyu/Package/AbhimanyuPractice.java
+++ Java_Programs/src/abhimanyu/Package/AbhimanyuPractice.java
@@ -4,6 +4,7 @@ public class AbhimanyuPractice {
 
 	public static void main(String[] args) {
 		System.out.println("Practice git stash method");
+		System.out.println("created feature branch!");
 	}
 
 }
